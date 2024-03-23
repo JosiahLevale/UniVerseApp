ca451bb327b9750a9839270ba02701404c5b7832
RomainTourv <rtourvieille@gmail.com>
2024-03-22 17:17:59 -0700

    InitialCommit

---
+++ calendar.js
@@ -0,0 +1,21 @@
+// Adding events to various categories - Romain
+// Add event to calendar -> Important
+// Atheltics
+// Chapel
+// TWUSA
+// Clubs
+
+// Calendar UI with added events - Oreo
+// Add classes to calendar
+
+// Overall app architecture - Oke
+// Home page
+// Calendar page
+// Explore events page
+
+// Design home page - Josiah
+// Bento Box design
+
+function goBack() {
+    window.history.back();
+}
