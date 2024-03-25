606b8c9bbbddfe6add07fb88bd3de8d7420a042c
Josiah Levale <josiahlevale.com>
2024-03-24 21:51:06 -0700

    login and home page

---
+++ home.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var items = document.querySelectorAll('.item');
+
+    items.forEach(function(item) {
+        item.addEventListener('click', function() {
+            this.classList.toggle('expanded');
+
+            var itemId = this.getAttribute('id');
+
+            if (this.classList.contains('expanded')) {
+                this.classList.add('new-content');
+            } else {
+                this.classList.remove('new-content');
+            }
+        });
+    });
+});
+
+
+const notifications = [
+    "HKIN 351 Online Today",
+    "CMPT 375 Cancelled",
+    "Movie Night now in Devries",
+];
+
+let currentNotificationIndex = 0;
+
+function showNotification(message) {
+    const notificationArea = document.getElementById('notification-area');
+    const notification = document.createElement('div');
+    notification.classList.add('notification');
+    notification.textContent = message;
+    notificationArea.appendChild(notification);
+
+    setTimeout(() => {
+        notification.remove();
+    }, 15000); 
+}
+
+function cycleNotifications() {
+    showNotification(notifications[currentNotificationIndex]);
+    currentNotificationIndex = (currentNotificationIndex + 1) % notifications.length;
+}
+
+cycleNotifications();
+
+setInterval(cycleNotifications, 10000);
+
